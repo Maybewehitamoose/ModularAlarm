61a3b5d77f0abdeebd9ae8a7d6067c89772fe413
Daryl Kurtz <me@darylkurtz.com>
2018-02-21 10:47:30 -0800

    1.01 - Added test class ModularAlarmAppWidgetProviderTest

---
+++ app/src/test/java/com/darylkurtz/modularalarm/ModularAlarmAppWidgetProviderTest.java
@@ -0,0 +1,19 @@
+package com.darylkurtz.modularalarm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dkurtz on 2/21/18.
+ */
+public class ModularAlarmAppWidgetProviderTest {
+    @Test
+    public void onUpdate() throws Exception {
+    }
+
+    @Test
+    public void onReceive() throws Exception {
+    }
+
+}
