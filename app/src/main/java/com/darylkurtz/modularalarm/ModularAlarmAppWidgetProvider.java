896d048855e80bb74efe840f2a9dc463f933d5c7
Daryl Kurtz <me@darylkurtz.com>
2018-02-20 21:11:02 -0800

    Initial commit

---
+++ app/src/main/java/com/darylkurtz/modularalarm/ModularAlarmAppWidgetProvider.java
@@ -0,0 +1,123 @@
+package com.darylkurtz.modularalarm;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.provider.AlarmClock;
+import android.text.format.DateFormat;
+import android.widget.RemoteViews;
+
+// ModularAlarmAppWidgetProvider is for the main widget component.
+
+public class ModularAlarmAppWidgetProvider extends AppWidgetProvider {
+    //Declare intents and actions for broadcast for button functionality
+    private static final String TAP_INTENT1 = "com.darylkurtz.modularalarm.BUTTON1_ACTION";
+    private final Intent tapIntent1 = new Intent( TAP_INTENT1 );
+
+    private static final String TAP_INTENT2 = "com.darylkurtz.modularalarm.BUTTON2_ACTION";
+    private final Intent tapIntent2 = new Intent( TAP_INTENT2 );
+
+    //Main Override for AppWidget interaction
+    @Override
+    public void onUpdate( Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds )
+    {
+        // Call the intents for the button actions
+        Intent button5Intent = new Intent(AlarmClock.ACTION_SHOW_ALARMS);
+
+        PendingIntent pendingButton1Intent = PendingIntent.getBroadcast(context, 0, tapIntent1, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingButton2Intent = PendingIntent.getBroadcast(context, 0, tapIntent2, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingButton3Intent = PendingIntent.getActivity(context, 0, button5Intent, 0);
+
+
+        // String to define the next scheduled alarm
+        String nextAlarm = getNextAlarmString( context );
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews( context.getPackageName(), R.layout.widget_layout );
+        views.setTextViewText( R.id.alarm_text, nextAlarm );
+
+        // onClick events for the buttons
+        views.setOnClickPendingIntent( R.id.button1, pendingButton1Intent );
+        views.setOnClickPendingIntent( R.id.button2, pendingButton2Intent );
+        views.setOnClickPendingIntent( R.id.button3, pendingButton3Intent );
+
+        // There may be multiple widgets active, so update all of them
+        for ( int appWidgetId : appWidgetIds ) {
+            appWidgetManager.updateAppWidget( appWidgetId, views );
+        }
+    }
+
+    //onReceive override for broadcasts
+    @Override
+    public void onReceive( Context context, Intent intent )
+    {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance( context );
+        ComponentName thisAppWidget = new ComponentName( context.getPackageName(), getClass().getName() );
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds( thisAppWidget );
+
+        onUpdate( context, appWidgetManager, appWidgetIds );
+
+        // TODO (3) Update functions below after settings panel is implemented to allow for custom alarm variable
+        //Determine current time and add two hours, then set alarm
+        if ( TAP_INTENT1.equals( intent.getAction() ) ) {
+            Integer plusOneHour = getCurrentHour() + 2;
+            Integer carryTheMinutes = getCurrentMinutes();
+            Intent setAlarm1Hour = new Intent(AlarmClock.ACTION_SET_ALARM);
+            setAlarm1Hour.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+            setAlarm1Hour.putExtra(AlarmClock.EXTRA_HOUR, plusOneHour);
+            setAlarm1Hour.putExtra(AlarmClock.EXTRA_MINUTES, carryTheMinutes);
+            context.startActivity(setAlarm1Hour);
+        }
+
+        //Determine current time and add four hours, then set alarm
+        if ( TAP_INTENT2.equals( intent.getAction() ) ) {
+            Integer plusOneHour = getCurrentHour() + 4;
+            Integer carryTheMinutes = getCurrentMinutes();
+            Intent setAlarm1Hour = new Intent(AlarmClock.ACTION_SET_ALARM);
+            setAlarm1Hour.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+            setAlarm1Hour.putExtra(AlarmClock.EXTRA_HOUR, plusOneHour);
+            setAlarm1Hour.putExtra(AlarmClock.EXTRA_MINUTES, carryTheMinutes);
+            context.startActivity(setAlarm1Hour);
+        }
+
+    }
+
+    //Get the current hour from Calendar
+    private static Integer getCurrentHour() {
+        Integer currentHour;
+        currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        return currentHour;
+    }
+
+    //Get the current minutes from Calendar
+    private static Integer getCurrentMinutes() {
+        Integer currentMinutes;
+        currentMinutes = Calendar.getInstance().get(Calendar.MINUTE);
+        return currentMinutes;
+    }
+
+    // Determine the closest set alarm, if no alarm is set display a blank time.
+    private static String getNextAlarmString( Context context )
+    {
+        String res = null;
+        AlarmManager am = (AlarmManager)context.getSystemService( Context.ALARM_SERVICE );
+
+        AlarmManager.AlarmClockInfo nextAlarmClock = am.getNextAlarmClock();
+        if ( nextAlarmClock != null ) {
+            // Format alarm time as e.g. "Di. 06:30"
+            res = DateFormat.format("EEE HH:mm", nextAlarmClock.getTriggerTime() ).toString();
+        }
+
+        if ( ( res == null ) || res.isEmpty() ) {
+            res = context.getString( R.string.alarm_time );
+        }
+
+        return res;
+    }
+
+}
